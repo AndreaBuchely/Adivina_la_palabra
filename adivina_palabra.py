8030eab5de5a3f4419e2881c2cb01d0f078fff59
AndreaBuchely <abuchelymoreno@gmail.com>
2023-07-21 20:00:22 -0500

    reemplaza letra encontrada e inicia el bucle

--- adivina_palabra.py
+++ adivina_palabra.py
@@ -1,34 +1,30 @@
 print("Bienvenid@ al Juego")
-print("Tu objetivo es adivina la palabra oculta, para lo cual tienes 5 intentos")
+print("Tu objetivo es adivinar la palabra oculta, para lo cual tienes 8 intentos")
 
 from random import choice
 
 palabras = ["bosnia", "brasil", "tambien", "oculta", "precio"]
 letras_incorrectas = []
 letras_correctas = []
-intentos = 5
+intentos = 8
 aciertos = 0
 juego_terminado = False
+lista_oculta = []
+lista_letras = []
 
 def elegir_palabra(lista_palabras):
     palabra_elegida = choice(lista_palabras)
 
     return palabra_elegida
 
-palabra = elegir_palabra(palabras)
-
-
 def mostrar_nuevo_tablero(palabra_elegida):
-    lista_oculta =[]
+    
     
     for l in palabra_elegida:
         lista_oculta.append("-")
-    
+        lista_letras.append(l)
+            
     print(lista_oculta)
-
-palabra_elegida = elegir_palabra(palabras)
-
-mostrar_nuevo_tablero(palabra_elegida)
 
 def validacion_dato(mi_valor):
     letras_validas = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z"
@@ -37,20 +33,43 @@ def validacion_dato(mi_valor):
     else:
         print("El valor ingresado debe ser una letra")
 
-letra_a_validar = input("Ingrese una letra: ")
-letra_a_validar.lower()
-letra_a_validar.strip()
-validacion_dato(letra_a_validar)
-
-def buscarla_en_palabra (letra_a_validar):
-    if letra_a_validar in palabra_elegida:
+def  buscarla_en_palabra (letra_a_validar):
+    if  letra_a_validar  in  palabra_elegida :
        letras_correctas.append(letra_a_validar)
     else:
        letras_incorrectas.append(letra_a_validar)
-         
-buscarla_en_palabra(letra_a_validar)
-print(f'LETRAS INCORRECTAS:{letras_incorrectas}')
-print(f'LETRAS CORRECTAS:{letras_correctas}')
+
+def reemplazar_valor(letra_a_validar):
+    i = 0
+    
+    for valor in lista_letras:
+      if valor == letra_a_validar:
+        lista_oculta[i] = letra_a_validar
+      i +=1  
+        
+    print(lista_oculta)
+
+palabra_elegida = elegir_palabra(palabras)
+
+mostrar_nuevo_tablero(palabra_elegida)
+
+while intentos > 0:
+  letra_a_validar = input("Ingrese una letra: ")
+  letra_a_validar.lower()
+  letra_a_validar.strip()
+  buscarla_en_palabra(letra_a_validar)
+  if letra_a_validar in palabra_elegida:
+        aciertos +=1
+        reemplazar_valor(letra_a_validar)
+  else:
+        intentos -=1
+  print(f'LETRAS INCORRECTAS:{letras_incorrectas}')
+  print(f'LETRAS CORRECTAS:{letras_correctas}')
+
+  break
+
+
+
 
 
 
